ccf23f884a14253e788ee32f3371d54fa8e9d1b2
Gerry <weegerryogle@protonmail.com>
2024-06-25 11:02:15 +0100

    starting to form page

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('scroll', function() {
-    let scrollPosition = window.pageYOffset;
-    document.querySelectorAll('.parallax').forEach(function(parallaxElem) {
-        let rate = parallaxElem.getBoundingClientRect().top * 0.5;
-        parallaxElem.style.transform = `translateY(${rate}px)`;
-    });
-});
